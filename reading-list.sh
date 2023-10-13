226ffdb2d7ad4890d23ef29aebf2b42eb55ca671
Spencer Heywood <github@heywoodlh.io>
2023-10-13 15:08:14 -0600

    updated reading-list.sh styling

--- reading-list.sh
+++ reading-list.sh
@@ -12,4 +12,4 @@ printf '\nMy tech reading list generated weekly.\n' >> ${file}
 printf 'I use [Miniflux](https://miniflux.app/) to aggregate my feeds in combination with [Reeder5](https://www.reederapp.com/) on mobile.\n' >> ${file}
 printf "\nGenerated on $(date +%D)\n\n" >> ${file}
 
-curl --silent -H "X-Auth-Token: ${MINIFLUX_API_TOKEN}" "${MINIFLUX_FEED_URL}" | jq -r '.[] | "---\nTitle: " + .title,"URL: " + .site_url,"RSS: " + .feed_url + "\n"' | grep -v '.tailscale' >> ${file}
+curl --silent -H "X-Auth-Token: ${MINIFLUX_API_TOKEN}" "${MINIFLUX_FEED_URL}" | jq -r '.[] | "---\nTitle: " + .title,"\nURL: " + .site_url,"\nRSS: " + .feed_url + "\n"' | grep -v '.tailscale' >> ${file}
