8bc20315cd3bbd9fa6e9418ad2fe9246a7492c05
Spencer Heywood <github@heywoodlh.io>
2024-04-23 17:48:36 -0600

    updated reading-list.sh style

--- reading-list.sh
+++ reading-list.sh
@@ -10,6 +10,6 @@ echo '---' > ${file}
 printf 'layout: page\ntitle: Reading List\npermalink: /reading-list/\n' >> ${file}
 echo '---' >> ${file}
 printf '\nMy tech reading list generated weekly via [reading-list.yml](https://github.com/heywoodlh/heywoodlh.io/blob/main/.github/workflows/reading-list.yml).\n' >> ${file}
-printf "\nGenerated on $(date +%D)\n\n" >> ${file}
+printf "\n<div class="date">\nGenerated on $(date +%D)\n</div>\n" >> ${file}
 
 curl --silent -H "X-Auth-Token: ${MINIFLUX_API_TOKEN}" "${MINIFLUX_FEED_URL}" | jq -r '.[] | "---\nTitle: " + .title,"\nURL: <" + .site_url + ">","\nRSS: <" + .feed_url + ">\n"' | grep -vE '.tailscale|ts\.net|rsshub' >> ${file}
